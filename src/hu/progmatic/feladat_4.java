a112450432847e6e17f100fe013bfb4a23a56b4b
kimbi91 <subscribefan@gmail.com>
2022-03-12 17:12:27 +0100

    feladat 4 resz 1

--- src/hu/progmatic/feladat_4.java
+++ src/hu/progmatic/feladat_4.java
@@ -7,6 +7,12 @@ public class feladat_4 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        System.out.println("Kérem az első számot!");
+        int firstNumber = sc.nextInt();
+        System.out.println("Kérem a második számot!");
+        int secondNumber = sc.nextInt();
+        sc.nextLine();
 
+        boolean firstPlace = firstNumber > secondNumber;
     }
 }
