1d2d915c3b9be191a4a4f0b90269120ab8838402
Nagyandb <nagyandb@gmail.com>
2022-03-12 14:52:03 +0100

    első feladat

--- src/hu/progmatic/Main.java
+++ src/hu/progmatic/Main.java
@@ -1,9 +1,19 @@
 package hu.progmatic;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Első githubkodásom!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Kérek egy évet!");
+        int ev = scanner.nextInt();
+        scanner.nextLine();
+
+        if (ev >= 1582) {
+
+        }
     }
 
 }
