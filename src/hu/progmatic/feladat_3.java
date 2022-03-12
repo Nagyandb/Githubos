9e928e17cdd7dad60daf36509e778bebcc4fd058
kimbi91 <subscribefan@gmail.com>
2022-03-12 16:54:08 +0100

    feladat 3 váz

---
+++ src/hu/progmatic/feladat_3.java
@@ -0,0 +1,16 @@
+package hu.progmatic;
+
+import java.util.Scanner;
+
+public class feladat_3 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Adj meg egy számot!");
+        int number = sc.nextInt();
+        sc.nextLine();
+
+    }
+}
