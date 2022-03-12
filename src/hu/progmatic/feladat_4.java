7edac451f19c30ad3cf90c8f6f1a39b7a3d12b8e
kimbi91 <subscribefan@gmail.com>
2022-03-12 17:05:11 +0100

    feladat 4

---
+++ src/hu/progmatic/feladat_4.java
@@ -0,0 +1,12 @@
+package hu.progmatic;
+
+import java.util.Scanner;
+
+public class feladat_4 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
