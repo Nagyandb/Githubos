2a0ff8430432562a48c99baa8e8c82886d5adf70
kimbi91 <subscribefan@gmail.com>
2022-03-12 16:32:59 +0100

    feladat 2 folyamat

--- src/hu/progmatic/feladat_2.java
+++ src/hu/progmatic/feladat_2.java
@@ -1,7 +1,25 @@
 package hu.progmatic;
 
+import java.util.Scanner;
+
 public class feladat_2 {
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Adj meg egy számot!");
+        int number = sc.nextInt();
+        sc.nextLine();
+
+        int index = 1;
+        int[] osztok = new int[number];
+
+        while (index < number) {
+            if (number % index == 0) {
+                osztok +=
+            }
+            index++;
+        }
 
     }
 }
