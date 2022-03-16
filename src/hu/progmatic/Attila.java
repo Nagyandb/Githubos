9161b520eef842d7a3d0d9343876fc13981ad10b
kimbi91 <subscribefan@gmail.com>
2022-03-16 19:43:11 +0100

    Számjegyösszegző

--- src/hu/progmatic/Attila.java
+++ src/hu/progmatic/Attila.java
@@ -1,4 +1,26 @@
 package hu.progmatic;
 
 public class Attila {
+
+    public static void main(String[] args) {
+
+        // Gyakorló 3.
+        // Számjegyösszegző
+
+        /*int number = -5483229;
+        int result = 0;
+
+        if (number < 0) {
+            number = number * -1;
+        }
+
+        while (number > 0) {
+            int digit = number % 10;
+            result += digit;
+            number /= 10;
+        }
+
+        System.out.println(result);*/
+
+    }
 }
